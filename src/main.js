5ebae88220acaeff3348827d6ba8cd2c93a50075
power_840 <liuyu19921017@gmail.com>
2021-07-28 21:33:57 +0800

    import export

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion: your project
+ * @version: 1.0
+ * @Author: power_840
+ * @Date: 2021-07-28 20:20:01
+ * @LastEditors: power_840
+ * @LastEditTime: 2021-07-28 21:07:25
+ */
 // console.log('hello');
 
 // let sum = (a, b) => a + b;
@@ -7,8 +15,16 @@
 // const _ = require('lodash');
 // console.log(_);
 
-import _ from 'lodash';
-import $ from 'jquery';
-console.log(_.concat([1, 2, 3], 4, 5));
-console.log($);
-export default 'main';
+// import _ from 'lodash';
+// import $ from 'jquery';
+// console.log(_.concat([1, 2, 3], 4, 5));
+// console.log($);
+// export default 'main';
+
+import { name, age } from "./msg";
+
+function say() {
+  console.log("hello", name);
+}
+
+say();
---
+++ src/msg.js
@@ -0,0 +1,10 @@
+/*
+ * @Descripttion: your project
+ * @version: 1.0
+ * @Author: power_840
+ * @Date: 2021-07-28 21:06:32
+ * @LastEditors: power_840
+ * @LastEditTime: 2021-07-28 21:06:51
+ */
+export var name = "aaa";
+export var age = 12;
